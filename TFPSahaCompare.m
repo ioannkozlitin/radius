cc7dd1aeefe751ed69e5a4525937810e6dcb9ed2
ioannkozlitin <ioannkozlitin@gmail.com>
2017-01-17 15:19:56 +0300

    Изменил критерий минимизации, добавил файлы для Fe

--- TFPSahaCompare.m
+++ TFPSahaCompare.m
@@ -7,7 +7,7 @@ run(sprintf("table_new_%d",Z));
 run(sprintf("table_saha_%d",Z));
 em = abs(log10(abs(P./P_TFP)));
 fprintf("\n");
-%e = mean(em(:).^2).^0.5
-e = max(em(:))
+e = mean(em(:).^2).^0.5
+%e = max(em(:))
 fflush(1);
 end
